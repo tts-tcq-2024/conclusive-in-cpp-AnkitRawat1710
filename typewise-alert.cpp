1009298f97ac66ccdd3a6c1cab9be0dc247aaaec
Ankit Rawat <84120496+AnkitRawat1710@users.noreply.github.com>
2024-10-31 23:32:09 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,63 +1,44 @@
+// typewise-alert.cpp
+
 #include "typewise-alert.h"
 #include <stdio.h>
 
-// Function to classify the temperature breach
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
     if (value < lowerLimit) {
         return TOO_LOW;
-    } else if (value > upperLimit) {
+    }
+    if (value > upperLimit) {
         return TOO_HIGH;
     }
     return NORMAL;
 }
 
-// Function to get cooling limits based on the cooling type
-void getCoolingLimits(CoolingType coolingType, int* lowerLimit, int* upperLimit) {
+void getCoolingLimits(CoolingType coolingType, int& lowerLimit, int& upperLimit) {
     switch (coolingType) {
         case PASSIVE_COOLING:
-            *lowerLimit = 0;
-            *upperLimit = 35;
+            lowerLimit = 0;
+            upperLimit = 35;
             break;
         case HI_ACTIVE_COOLING:
-            *lowerLimit = 0;
-            *upperLimit = 45;
+            lowerLimit = 0;
+            upperLimit = 45;
             break;
         case MED_ACTIVE_COOLING:
-            *lowerLimit = 0;
-            *upperLimit = 40;
+            lowerLimit = 0;
+            upperLimit = 40;
             break;
         default:
-            *lowerLimit = 0;
-            *upperLimit = 0;
+            // Handle unexpected cooling types
+            lowerLimit = 0;
+            upperLimit = 0; // Or set to some default error value
             break;
     }
 }
 
-// Function to check the temperature and send alerts accordingly
-void checkAndAlert(BatteryCharacter batteryChar, double temperatureInC) {
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
     int lowerLimit, upperLimit;
-    getCoolingLimits(batteryChar.coolingType, &lowerLimit, &upperLimit);
-    BreachType breachType = inferBreach(temperatureInC, lowerLimit, upperLimit);
-    sendAlert(breachType);
+    getCoolingLimits(coolingType, lowerLimit, upperLimit);
+    return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
-// Function to send alerts based on the breach type
-void sendAlert(BreachType breachType) {
-    switch (breachType) {
-        case TOO_LOW:
-            printf("Alert: Temperature too low!\n");
-            // Here you can implement sending to email/controller
-            break;
-        case TOO_HIGH:
-            printf("Alert: Temperature too high!\n");
-            // Here you can implement sending to email/controller
-            break;
-        case NORMAL:
-            printf("Temperature is normal.\n");
-            break;
-        default:
-            break;
-    }
-}
-
-
+// ... rest of the code remains unchanged
