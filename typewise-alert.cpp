25d1796eba34d452a4c6e0d53a6484f99a32196a
Ankit Rawat <84120496+AnkitRawat1710@users.noreply.github.com>
2024-10-31 22:01:21 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,86 +1,72 @@
 #include "typewise-alert.h"
 #include <stdio.h>
-#include <memory>
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-  if (value < lowerLimit) return TOO_LOW;
-  if (value > upperLimit) return TOO_HIGH;
+  if(value < lowerLimit) {
+    return TOO_LOW;
+  }
+  if(value > upperLimit) {
+    return TOO_HIGH;
+  }
   return NORMAL;
 }
 
-// Cooling Strategies
-class PassiveCooling : public ICoolingStrategy {
-public:
-  BreachType classifyTemperature(double temperature) const override {
-    return inferBreach(temperature, 0, 35);
+BreachType classifyTemperatureBreach(
+    CoolingType coolingType, double temperatureInC) {
+  int lowerLimit = 0;
+  int upperLimit = 0;
+  switch(coolingType) {
+    case PASSIVE_COOLING:
+      lowerLimit = 0;
+      upperLimit = 35;
+      break;
+    case HI_ACTIVE_COOLING:
+      lowerLimit = 0;
+      upperLimit = 45;
+      break;
+    case MED_ACTIVE_COOLING:
+      lowerLimit = 0;
+      upperLimit = 40;
+      break;
   }
-};
-
-class HiActiveCooling : public ICoolingStrategy {
-public:
-  BreachType classifyTemperature(double temperature) const override {
-    return inferBreach(temperature, 0, 45);
-  }
-};
-
-
-class MedActiveCooling : public ICoolingStrategy {
-public:
-  BreachType classifyTemperature(double temperature) const override {
-    return inferBreach(temperature, 0, 40);
-  }
-};
-
-// Alert Strategies
-class ControllerAlert : public IAlertStrategy {
-public:
-  void sendAlert(BreachType breachType) const override {
-    const unsigned short header = 0xfeed;
-    printf("%x : %x\n", header, breachType);
-  }
-};
-
-class EmailAlert : public IAlertStrategy {
-public:
-  void sendAlert(BreachType breachType) const override {
-    const char* recipient = "a.b@c.com";
-    switch (breachType) {
-      case TOO_LOW:
-        printf("To: %s\nHi, the temperature is too low\n", recipient);
-        break;
-      case TOO_HIGH:
-        printf("To: %s\nHi, the temperature is too high\n", recipient);
-        break;
-      case NORMAL:
-        break;
-    }
-  }
-};
-
-// Factory functions to select appropriate strategies
-std::unique_ptr<ICoolingStrategy> selectCoolingStrategy(CoolingType coolingType) {
-  switch (coolingType) {
-    case PASSIVE_COOLING: return std::make_unique<PassiveCooling>();
-    case HI_ACTIVE_COOLING: return std::make_unique<HiActiveCooling>();
-    case MED_ACTIVE_COOLING: return std::make_unique<MedActiveCooling>();
-  }
-  return nullptr;
+  return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
-std::unique_ptr<IAlertStrategy> selectAlertStrategy(AlertTarget alertTarget) {
-  switch (alertTarget) {
-    case TO_CONTROLLER: return std::make_unique<ControllerAlert>();
-    case TO_EMAIL: return std::make_unique<EmailAlert>();
+void checkAndAlert(
+    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+
+  BreachType breachType = classifyTemperatureBreach(
+    batteryChar.coolingType, temperatureInC
+  );
+
+  switch(alertTarget) {
+    case TO_CONTROLLER:
+      sendToController(breachType);
+      break;
+    case TO_EMAIL:
+      sendToEmail(breachType);
+      break;
   }
-  return nullptr;
 }
 
-void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
-  auto coolingStrategy = selectCoolingStrategy(batteryChar.coolingType);
-  auto alertStrategy = selectAlertStrategy(alertTarget);
+void sendToController(BreachType breachType) {
+  const unsigned short header = 0xfeed;
+  printf("%x : %x\n", header, breachType);
+}
 
-  if (coolingStrategy && alertStrategy) {
-    BreachType breachType = coolingStrategy->classifyTemperature(temperatureInC);
-    alertStrategy->sendAlert(breachType);
+void sendToEmail(BreachType breachType) {
+  const char* recepient = "a.b@c.com";
+  switch(breachType) {
+    case TOO_LOW:
+      printf("To: %s\n", recepient);
+      printf("Hi, the temperature is too low\n");
+      break;
+    case TOO_HIGH:
+      printf("To: %s\n", recepient);
+      printf("Hi, the temperature is too high\n");
+      break;
+    case NORMAL:
+      break;
   }
 }
+
