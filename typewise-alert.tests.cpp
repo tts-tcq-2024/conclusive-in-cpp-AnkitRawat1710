165dbfd494162953ce21d83527334f36cc27698e
Ankit Rawat <84120496+AnkitRawat1710@users.noreply.github.com>
2024-11-01 12:48:39 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,16 +1,32 @@
+// typewise-alert.tests.cpp
+#include <gtest/gtest.h>
 #include "typewise-alert.h"
-#include <cassert>
-#include <iostream>  // Include this to use std::cout
 
-void testCheckAndAlert() {
-    BatteryCharacter batteryChar = {PASSIVE_COOLING};
-    checkAndAlert(TO_CONTROLLER, batteryChar, -5.0); // Expected to send TOO_LOW alert
-    checkAndAlert(TO_EMAIL, batteryChar, 50.0);      // Expected to send TOO_HIGH alert
-    checkAndAlert(TO_EMAIL, batteryChar, 25.0);      // Expected to send NORMAL alert
+TEST(TypeWiseAlertTestSuite, InfersBreachAccordingToLimits) {
+  EXPECT_EQ(inferBreach(30, 20, 40), BreachType::NORMAL);
+  EXPECT_EQ(inferBreach(15, 20, 40), BreachType::TOO_LOW);
+  EXPECT_EQ(inferBreach(45, 20, 40), BreachType::TOO_HIGH);
 }
 
-int main() {
-    testCheckAndAlert();
-    std::cout << "All tests passed!\n";
-    return 0;
+TEST(TypeWiseAlertTestSuite, ClassifiesTemperatureBreachForCoolingTypes) {
+  EXPECT_EQ(classifyTemperatureBreach(CoolingType::PASSIVE_COOLING, 20), BreachType::NORMAL);
+  EXPECT_EQ(classifyTemperatureBreach(CoolingType::PASSIVE_COOLING, 36), BreachType::TOO_HIGH);
+  EXPECT_EQ(classifyTemperatureBreach(CoolingType::HI_ACTIVE_COOLING, 46), BreachType::TOO_HIGH);
+  EXPECT_EQ(classifyTemperatureBreach(CoolingType::MED_ACTIVE_COOLING, 41), BreachType::TOO_HIGH);
+}
+
+TEST(TypeWiseAlertTestSuite, SendsAlertToController) {
+  ControllerAlert controllerAlert;
+  testing::internal::CaptureStdout();
+  controllerAlert.sendAlert(BreachType::TOO_HIGH);
+  std::string output = testing::internal::GetCapturedStdout();
+  EXPECT_NE(output.find("feed : 2"), std::string::npos);
+}
+
+TEST(TypeWiseAlertTestSuite, SendsAlertToEmail) {
+  EmailAlert emailAlert;
+  testing::internal::CaptureStdout();
+  emailAlert.sendAlert(BreachType::TOO_HIGH);
+  std::string output = testing::internal::GetCapturedStdout();
+  EXPECT_NE(output.find("To: a.b@c.com\nHi, the temperature is too high\n"), std::string::npos);
 }
