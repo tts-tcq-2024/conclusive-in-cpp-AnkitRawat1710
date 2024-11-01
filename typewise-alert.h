11836c8a292166fd9f24d7441786c952b7684306
Ankit Rawat <84120496+AnkitRawat1710@users.noreply.github.com>
2024-11-01 15:41:25 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,16 +1,42 @@
-// typewise-alert.h
 #pragma once
+
 #include <string>
 
-enum class CoolingType { PASSIVE_COOLING, HI_ACTIVE_COOLING, MED_ACTIVE_COOLING };
-enum class BreachType { NORMAL, TOO_LOW, TOO_HIGH };
-enum class AlertTarget { TO_CONTROLLER, TO_EMAIL };
+enum class CoolingType {
+  PASSIVE_COOLING,
+  HI_ACTIVE_COOLING,
+  MED_ACTIVE_COOLING
+};
+
+enum class BreachType {
+  NORMAL,
+  TOO_LOW,
+  TOO_HIGH
+};
+
+enum class AlertTarget {
+  TO_CONTROLLER,
+  TO_EMAIL
+};
 
 struct BatteryCharacter {
   CoolingType coolingType;
   std::string brand;
 };
 
+// Abstract base class for Breach Classification Strategy
+class IBreachClassifier {
+public:
+  virtual ~IBreachClassifier() = default;
+  virtual BreachType classify(double temperatureInC) const = 0;
+};
+
+// Abstract base class for Alert Strategy
+class IAlert {
+public:
+  virtual ~IAlert() = default;
+  virtual void send(BreachType breachType) const = 0;
+};
+
 BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 void checkAndAlert(AlertTarget alertTarget, const BatteryCharacter& batteryChar, double temperatureInC);
